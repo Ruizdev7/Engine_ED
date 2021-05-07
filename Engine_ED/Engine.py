82ca887ed9e0a41edb5c556a1c3b59aba2318a42
Joseph Ruiz <ruizdev7@outlook.com>
2021-05-07 09:17:36 -0500

    Inclucion de plantillas html para la autenticacion de cliente, empleado y otros cambios relacionados

--- Engine_ED/Engine.py
+++ Engine_ED/Engine.py
@@ -4,10 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
+@app.route('/registroCliente')
+def registroCliente():
+    return render_template('registroCliente.html')
+
+@app.route('/loginCliente')
+def loginCliente():
+    return render_template('loginCliente.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-
