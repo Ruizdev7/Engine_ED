e679c1d00cbc9cfcd1ecb40d15b72416c2027f35
Joseph Ruiz <ruizdev7@outlook.com>
2021-05-16 21:22:06 -0500

    Actualizacion 202105162122

--- Engine_ED/Engine.py
+++ Engine_ED/Engine.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+mysql= MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='123qweAs*'
+app.config['MYSQL_DATABASE_DB']='DB_ENGINE_ED'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -18,6 +26,18 @@ def loginCliente():
 @app.route('/baseModulos')
 def baseModulos():
     return render_template('baseModulos.html')
+
+@app.route('/moduloAdminSistema/registrarEmpleado')
+def registrarEmpleado():
+
+    #sql = "select * from tblEmpleado;"
+    #conn= mysql.connect()
+    #cursor=conn.cursor()
+    #cursor.execute(sql)
+    #con.commit()
+
+    return render_template('moduloAdminSistema/registrarEmpleado.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
