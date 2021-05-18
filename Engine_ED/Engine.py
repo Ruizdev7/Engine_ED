ece06f49e59361547c42b25b6e5170fdfbaebedb
Joseph Ruiz <ruizdev7@outlook.com>
2021-05-17 23:57:32 -0500

    Actualizacion sobre formulario de registro de Empleado usando python y mysql-connector-python 202105172357

--- Engine_ED/Engine.py
+++ Engine_ED/Engine.py
@@ -1,15 +1,7 @@
 from flask import Flask
 from flask import render_template
-from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-
-mysql= MySQL()
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']='123qweAs*'
-app.config['MYSQL_DATABASE_DB']='DB_ENGINE_ED'
-mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -29,15 +21,7 @@ def baseModulos():
 
 @app.route('/moduloAdminSistema/registrarEmpleado')
 def registrarEmpleado():
-
-    #sql = "select * from tblEmpleado;"
-    #conn= mysql.connect()
-    #cursor=conn.cursor()
-    #cursor.execute(sql)
-    #con.commit()
-
     return render_template('moduloAdminSistema/registrarEmpleado.html')
     
-
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
