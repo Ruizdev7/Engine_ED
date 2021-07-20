8baff938f50a5ddeb2052304510a954bd9a1fc91
Joseph Ruiz <ruizdev7@outlook.com>
2021-07-20 16:34:55 -0500

    Se crea el sistema de registro y autenticacion de clientes incluyendo las validaciones de los formularios ademas de proteccion CSFR para los formularios e inclucion de la creacion de las sesiones para acceso al portal transaccional de cliente

--- Engine_ED/db.py
+++ Engine_ED/db.py
@@ -1,15 +1,8 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="123qweAs*",
-    database="DB_ENGINE_ED"
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='123qweAs*',
+    db='Engine_DB'
 )
-
-cursor = db.cursor()
-cursor.execute("SELECT * FROM tblRoles")
-result = cursor.fetchall()
-
-for i in result:
-    print(i)
