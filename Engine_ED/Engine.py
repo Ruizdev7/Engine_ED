73eee9f1eb1147ae96b7ba0dd10d3204c89c42fa
Joseph Ruiz <ruizdev7@outlook.com>
2021-07-27 23:19:58 -0500

    Progreso del crud de empleados no completo

--- Engine_ED/Engine.py
+++ Engine_ED/Engine.py
@@ -17,54 +17,12 @@ app.config["SECRET_KEY"] = 'hardsecretkey'
 csfr = CSRFProtect(app)
 
 
-@app.errorhandler(404)
-def paginaNoEncontrada(e):
-    pass
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/ingresoCliente', methods = ['GET','POST'])
-def ingresoCliente():
-    form = forms.ingresoCliente(request.form)
-    if request.method == 'POST':
-        email = request.form['correoElectronicoCliente']
-        contraseña = request.form['contraseñaCliente']
-        mycursor = mydb.cursor()
-        sql = "select ccnCliente, primerNombreCliente, PrimerApellidoCliente, correoElectronicoCliente, contraseñaCliente from `Engine_DB`.`tblCliente` where correoElectronicoCliente = %s"
-        val = (request.form['correoElectronicoCliente'],)
-        mycursor.execute(sql, val)
-        resultado = mycursor.fetchone()
-        print(resultado)
-#asynawait in javascrip para python
-        if email == resultado[3] and check_password_hash(resultado[4], contraseña):
-            session['username'] = request.form['correoElectronicoCliente']
-            mensajeExito = 'Bienvenido {}'.format(resultado[1] + ' ' + resultado[2])
-            flash(mensajeExito)
-            return redirect(url_for('portalTransaccional'))
-        else:
-            mensajeError = 'Email o Contraseña incorrecta porfavor intente de nuevo'
-            flash(mensajeError)
-            return redirect(url_for('ingresoCliente'))
-    else:
-        return render_template('ingresoCliente.html', form = form)
-
-@app.route('/portalTransaccional', methods = ['GET','POST'])
-def portalTransaccional():
-    #Identificar si el usuario esta conectado atravez de sesiones
-    if 'username' in session:
-        return render_template('portalTransaccional/homePortalTransaccional.html')
-
-
-@app.route('/cerrarSesion', methods = ['GET','POST'])
-def cerrarSesion():
-    session.pop('username')
-    return redirect(url_for('index'))
-    
-
+#***********************************************ZONA CLIENTE*************************************************
 @app.route('/registroCliente', methods = ['GET','POST'])
 def registroCliente():
     form = forms.registroCliente(request.form)
@@ -133,6 +91,139 @@ def registroCliente():
         mydb.close()
 
         return redirect(url_for('ingresoCliente'))
+
+
+@app.route('/ingresoCliente', methods = ['GET','POST'])
+def ingresoCliente():
+    form = forms.ingresoCliente(request.form)
+    if request.method == 'POST':
+        email = request.form['correoElectronicoCliente']
+        contraseña = request.form['contraseñaCliente']
+        mycursor = mydb.cursor()
+        sql = "select ccnCliente, primerNombreCliente, PrimerApellidoCliente, correoElectronicoCliente, contraseñaCliente from `Engine_DB`.`tblCliente` where correoElectronicoCliente = %s"
+        val = (request.form['correoElectronicoCliente'],)
+        mycursor.execute(sql, val)
+        resultado = mycursor.fetchone()
+        print(resultado)
+        if email == resultado[3] and check_password_hash(resultado[4], contraseña):
+            session['username'] = request.form['correoElectronicoCliente']
+            mensajeExito = 'Bienvenido {}'.format(resultado[1] + ' ' + resultado[2])
+            flash(mensajeExito)
+            return redirect(url_for('portalTransaccional'))
+        else:
+            mensajeError = 'Email o Contraseña incorrecta porfavor intente de nuevo'
+            flash(mensajeError)
+            return redirect(url_for('ingresoCliente'))
+    else:
+        return render_template('ingresoCliente.html', form = form)
+
+
+@app.route('/portalTransaccional', methods = ['GET','POST'])
+def portalTransaccional():
+    #Identificar si el usuario esta conectado atravez de sesiones
+    if 'username' in session:
+        return render_template('portalTransaccional/homePortalTransaccional.html')
+
+
+@app.route('/cerrarSesion', methods = ['GET','POST'])
+def cerrarSesion():
+    session.pop('username')
+    return redirect(url_for('index'))
+#*********************************************FIN ZONA CLIENTE***********************************************    
+
+#***********************************************ZONA EMPLEADO*************************************************
+@app.route('/dbEmpleados')
+def dbEmpleados():
+    mycursor = mydb.cursor()
+    mycursor.execute("select * from `Engine_DB`.`tblEmpleado`;")
+    listaEmpleados = mycursor.fetchall()
+    print(listaEmpleados)
+    return render_template('moduloAdminSistema/dbEmpleados.html', listaEmpleados = listaEmpleados)
+
+
+@app.route('/registrarEmpleado', methods = ['GET','POST'])
+def registrarEmpleado():
+    form = forms.registrarEmpleado(request.form)
+    if request.method == 'GET':
+        return render_template('moduloAdminSistema/registrarEmpleado.html', form = form)
+
+    else:
+        numeroidentificacion = request.form['numeroIdEmpleado']
+        tipoIdentificacion = request.form['tipoId']
+        fechaExpedicionIdentificacion = request.form['fechaExpIdEmpleado']
+        lugarExpedicionIdentificacion = request.form['lugarExpIdEmpleado']
+        primerNombre = request.form['primerNombreEmpleado']
+        segundoNombre = request.form['segundoNombreEmpleado']
+        primerApellido = request.form['primerApellidoEmpleado']
+        segundoApellido = request.form['segundoApellidoEmpleado']
+        fechaNacimiento = request.form['fechaNacimientoEmpleado']
+        direccionResidencia = request.form['direccionEmpleado']
+        barrioResidencia = request.form['barrioEmpleado']
+        paisResidencia = request.form['idPais']
+        departamentoResidencia = request.form['idDepartamento']
+        municipioResidencia = request.form['idMunicipio']
+        celular = request.form['celularEmpleado']
+        email = request.form['correoElectronicoEmpleado']
+
+        mycursor = mydb.cursor()
+        sql = """insert into `Engine_DB`.`tblEmpleado` (
+        `ccnEmpleado`,
+        `numeroIdEmpleado`,
+        `tipoId`,
+        `fechaExpIdEmpleado`,
+        `lugarExpIdEmpleado`,
+        `primerNombreEmpleado`,
+        `segundoNombreEmpleado`,
+        `primerApellidoEmpleado`,
+        `segundoApellidoEmpleado`,
+        `fechaNacimientoEmpleado`,
+        `direccionEmpleado`,
+        `barrioEmpleado`,
+        `idPais`,
+        `idDepartamento`,
+        `idMunicipio`,
+        `celularEmpleado`,
+        `correoElectronicoEmpleado`) values (null, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+
+        datos = (numeroidentificacion,
+        tipoIdentificacion,
+        fechaExpedicionIdentificacion,
+        lugarExpedicionIdentificacion,
+        primerNombre,
+        segundoNombre,
+        primerApellido,
+        segundoApellido,
+        fechaNacimiento,
+        direccionResidencia,
+        barrioResidencia,
+        paisResidencia,
+        departamentoResidencia,
+        municipioResidencia,
+        celular,
+        email)
+
+        mycursor.execute(sql, datos)
+        mydb.commit()
+        mydb.close()
+        return render_template('index.html')
+
+
+@app.route('/eliminarEmpleado/<int:ccnEmpleado>')
+def eliminarEmpleado(ccnEmpleado):
+    mycursor = mydb.cursor()
+    mycursor.execute("delete from `Engine_DB`.`tblEmpleado` where ccnEmpleado = %s", (ccnEmpleado,)) 
+    mydb.commit()
+    return redirect(url_for('dbEmpleados'))
+
+
+@app.route('/editarEmpleado/<int:ccnEmpleado>')
+def editarEmpleado(ccnEmpleado):
+    form = forms.registrarEmpleado(request.form)
+    mycursor = mydb.cursor()
+    mycursor.execute("select * from `Engine_DB`.`tblEmpleado` where ccnEmpleado = %s", (ccnEmpleado,))
+    empleado = mycursor.fetchone()
+    print(empleado)
+    return render_template('moduloAdminSistema/editarEmpleado.html', empleado = empleado, form = form)
 
 
 if __name__ == '__main__':
